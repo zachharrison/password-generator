79e314d6297290462bfa9feb9301dc7265c27f63
Zach Harrison <zharrison@shaw.ca>
2021-07-05 21:32:30 -0700

    Copy passwords to clipboard and save to seperate file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const program = require('commander');
-const createPassword = require('./utils/createPassword');
 const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+const createPassword = require('./utils/createPassword');
+const savePassword = require('./utils/savePassword');
 
 const log = console.log;
 
@@ -18,5 +20,12 @@ const { length, save, numbers, symbols } = program.opts();
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols);
 
+// Save to file
+if (save) savePassword(generatedPassword);
+
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
 // Output generated password
 log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Password copied to clipboard'));
